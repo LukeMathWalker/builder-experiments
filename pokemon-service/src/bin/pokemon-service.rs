c94f47ea565a8885a4dbf216d0b136add864db57
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-10-15 09:08:50 +0200

    Use new builder.

--- pokemon-service/src/bin/pokemon-service.rs
+++ pokemon-service/src/bin/pokemon-service.rs
@@ -6,13 +6,13 @@
 // This program is exported as a binary named `pokemon-service`.
 use std::{net::SocketAddr, sync::Arc};
 
-use aws_smithy_http_server::{routing::Router, AddExtensionLayer};
+use aws_smithy_http_server::{AddExtensionLayer};
 use clap::Parser;
 use pokemon_service::{
     capture_pokemon, check_health, do_nothing, get_pokemon_species, get_server_statistics, get_storage, setup_tracing,
     State,
 };
-use pokemon_service_server_sdk::operation_registry::OperationRegistryBuilder;
+use pokemon_service_server_sdk::service::{PokemonService};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -29,25 +29,18 @@ struct Args {
 pub async fn main() {
     let args = Args::parse();
     setup_tracing();
-    let app: Router = OperationRegistryBuilder::default()
-        // Build a registry containing implementations to all the operations in the service. These
-        // are async functions or async closures that take as input the operation's input and
-        // return the operation's output.
+    let app = PokemonService::builder()
         .get_pokemon_species(get_pokemon_species)
         .get_storage(get_storage)
         .get_server_statistics(get_server_statistics)
         .capture_pokemon(capture_pokemon)
         .do_nothing(do_nothing)
         .check_health(check_health)
-        .build()
-        .expect("Unable to build operation registry")
-        // Convert it into a router that will route requests to the matching operation
-        // implementation.
-        .into();
+        .build();
 
     // Setup shared state and middlewares.
     let shared_state = Arc::new(State::default());
-    let app = app.layer(AddExtensionLayer::new(shared_state));
+    let app = app.layer(&AddExtensionLayer::new(shared_state));
 
     // Start the [`hyper::Server`].
     let bind: SocketAddr = format!("{}:{}", args.address, args.port)
